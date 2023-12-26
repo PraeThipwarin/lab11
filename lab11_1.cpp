083c32dd49ee85e019add37ca6ee835dcd917877
PraeThipwarin <prae.tippy@gmail.com>
2023-12-26 13:40:52 +0700

    redo lab11_2

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,39 +1,25 @@
 #include<iostream>
-#include<string>
 #include<cstdlib>
 #include<ctime>
 using namespace std;
 
 int main()
 {
-    srand(time(0));
-    int x = rand()%9;
-    string grade;
-    if (x == 0)
-        grade = "A";
-    else if (x == 1)
-        grade ="B+";
-    else if (x == 2)
-        grade ="B";
-    else if (x == 3)
-        grade ="C+";
-    else if (x == 4)
-        grade ="C";
-    else if (x == 5)
-        grade ="D+";
-    else if (x == 6)
-        grade ="D";
-    else if (x == 7)
-        grade ="F";
-    else
-        grade ="W";
-    
-    cout << "Press Enter 3 times to reveal your future. \n";
+    cout << "Press Enter 3 times to reveal your future.";
     cin.get(); 
     cin.get(); 
     cin.get();
-    cout << "You will get " << grade << " in this 261102.";
-
+    
+    srand(time(0));
+    int x = rand()%9;
+    if(x == 0) cout << "You will get A in this 261102.";
+    else if(x == 1) cout << "You will get B+ in this 261102.";
+    else if(x == 2) cout << "You will get B in this 261102.";
+    else if(x == 3) cout << "You will get C+ in this 261102.";
+    else if(x == 4) cout << "You will get C in this 261102.";
+    else if(x == 5) cout << "You will get D+ in this 261102.";
+    else if(x == 6) cout << "You will get D in this 261102.";
+    else if(x == 7) cout << "You will get F in this 261102.";
+    else cout << "You will get W in this 261102.";
     return 0;
 }
-
